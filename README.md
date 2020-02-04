bfa6758efa1bf2d6270c6e5584cac6d6cc827b51
tpkn <tpkn@users.noreply.github.com>
2020-02-04 20:36:53 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# slack-bot
+# Sava
+
+
+## API
+
+```php
+new SlackBot($channel, $access_token[, $service_url]);
+```
+
+
+
+## Usage
+
+```php
+include 'slack-bot.php';
+
+$bot = new SlackBot('XXXXXXXXX', 'xoxb-AAAAAAAAAAAA-BBBBBBBBBBBB-CCCCCCCCCCCCCCCCCCCCCCCC');
+$response = $bot->says('chiki-briki!');
+```
+
+
+
+### @return
+
+```
+Array
+(
+   [ok] => 1
+   [channel] => XXXXXXXXX
+   [ts] => 1580801069.000900
+   [message] => Array
+      (
+         [bot_id] => XXXXXXXXX
+         [type] => message
+         [text] => chiki-briki!
+         [user] => BBBBBBBBB
+         [ts] => 1580801069.000900
+         [team] => UUUUUUUUU
+         [bot_profile] => Array
+            (
+               [id] => XXXXXXXXX
+               [deleted] => 
+               [name] => my-reports
+               [updated] => 1580221012
+               [app_id] => HHHHHHHHH
+               [team_id] => UUUUUUUUU
+            )
+      )
+)
+```
+
+```
+Array
+(
+   [ok] => 
+   [error] => invalid_auth
+)
+```
+
---
+++ slack-bot.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Slack Bot
+ */
+class SlackBot {
+   public $channel;
+   public $access_token;
+   public $service_url = 'https://slack.com/api/chat.postMessage';
+
+   function __construct($channel, $access_token, $service_url = null){
+      $this->channel = $channel;
+      $this->access_token = $access_token;
+
+      if(isset($service_url)){
+         $this->service_url = $service_url;
+      }
+   }
+
+   public function says($text) {
+      $body = array(
+         'text' => $text,
+         'channel' => $this->channel
+      );
+
+      $headers = array(
+         'Content-Type: application/json; charset=utf-8',
+         'Authorization: Bearer ' . $this->access_token,
+      );
+
+      $curl = curl_init($this->service_url);
+      curl_setopt_array($curl, array(
+         CURLOPT_POST => true,
+         CURLOPT_RETURNTRANSFER => true,
+         CURLOPT_HTTPHEADER => $headers,
+         CURLOPT_POSTFIELDS => json_encode($body),
+      ));
+
+      $response = curl_exec($curl);
+
+      if ($response === false) {
+         die(curl_error($curl));
+      }
+
+      return json_decode($response, true);
+   }
+}
+
+?>
